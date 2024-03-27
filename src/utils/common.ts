6934d3603fa2afdaf3ec7ec760de9c776edf6776
blackheart7565 <give.me.heart8844@gmail.com>
2024-03-27 22:27:42 +0200

    Added: Method for converting bytes to the desired image size format.

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,17 @@
 
+export const convertByteToFormatImageSize = (byte: number) => {
+	const units: Array<string> = ['B', 'KB', 'MB', 'GB', 'TB'];
+	let unitsIndex: number = 0;
+	let size: number = byte;
+
+	while (size >= 1024 && unitsIndex < units.length - 1) {
+		size /= 1024;
+		unitsIndex++;
+	}
+
+	return `${size.toFixed(1)} ${units[unitsIndex]}`;
+};
+
 export const readFileAsDataURL = (image: File) => {
 	return new Promise((resolve, rejects) => {
 		const reader = new FileReader();
